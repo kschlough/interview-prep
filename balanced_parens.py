4c839d6b3f3f52adc22d5f399da732557ced4dac
Kate Schlough <krschlough@gmail.com>
2020-10-31 17:23:58 +0000

    Added unadded files to track

---
+++ balanced_parens.py
@@ -0,0 +1,27 @@
+# test problem balanced parens
+
+# openers = ['(', '{', '[' ]
+# closers = [ ')', '}', ']' ]
+
+# open_count = 0
+# close_count = 0
+
+# def balanced_paren(string):
+    
+#     openers = ['(', '{', '[' ]
+#     closers = [ ')', '}', ']' ]
+#     open_count = 0
+#     close_count = 0
+#     parens = [string] 
+#     for paren in parens:
+#         print(paren)
+#         if paren in openers:
+#             open_count +=1
+#         elif paren in closers:
+#             close_count += 1
+#     print(open_count)
+#     print(close_count)
+#     return open_count == close_count 
+
+# balanced_paren("{ [ ] ( ) }")
+
---
+++ linked_lists.py
@@ -0,0 +1,55 @@
+# bare minimum linked list: only has a class for node
+class ListNode:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# linked list class keeps track of head, then ListNode tells us where to find the next node
+class LinkedList:
+    def __init__(self, head_node):
+        self.head_node = head_node
+
+# these two classes are singly linked list, each node points to one following it
+
+def merge_linked_lists(self, sorted_list1_head_node, sorted_list2_head_node):
+    
+    merged_head = None # this is what will be returned
+    current = None # track where you are in the list
+    
+    if sorted_list1_head_node < sorted_list2_head_node:
+        merged_linked_lists = ListNode(sorted_list1_head_node)
+    else:
+        merged_linked_lists = ListNode(sorted_list2_head_node)
+
+
+
+
+# first_node = ListNode(3)
+# -- value 3, next none
+# second_node = ListNode(3, third)
+# third = ListNode(5, fourth)
+# assume not empty, at least one node in each list
+# one can be longer than the other
+
+###########################################
+# implementation of the linked list
+# then test out doubly linked list
+# draw.io tool can be useful
+
+# each node points not just to next node but also to previous node
+
+class DoubleLinkedList:
+    # have head and tail
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+
+class DoubleListNode:
+    # prev and next
+    def __init__(self, value, previous=None, next=None):
+        self.value = value
+        self.previous = previous
+        self.next = next
+
+
+
