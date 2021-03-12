dc3ae0fa77643cce3f131b20f2fd3a9460454e26
kschlough <61514488+kschlough@users.noreply.github.com>
2021-03-12 11:38:03 -0800

    working on ctci linked list remove dupes problem

---
+++ ll_remove_dupes.py
@@ -0,0 +1,62 @@
+# ctci linked lists chapter
+
+# write code to remove duplicates from an unsorted linked list
+
+# pseudocode
+# create a dictionary to store all the values that we come across as keys
+# as values, implement a counter
+# go through the list once to get the # of times each appears
+# then go through the list one more time and remove the dupes by changing pointers
+
+# build the test linked list
+class Node: 
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+headnode = Node(9)
+val1 = Node(10)
+head.next = val1
+val2 = Node(234)
+val1.next = val2
+val3 = Node(9)
+val2.next = val3
+val4 = Node(3)
+val3.next = val4
+
+
+def ll_remove_dupes(head):
+    dupes_dict = {}
+    current = head
+
+    # check if there's no list
+    if head == None:
+        return None
+
+    while current.next:
+        if current.data in dupes_dict:
+            dupes_dict[current.data] += 1
+        else:
+            dupes_dict[current.data] = 1
+
+    # reset to start loop again, this time with 2 pointers
+    
+    # check if you need to change the head first, do this until you don't
+    while dupes_dict[head] > 1:
+        head = head.next
+    
+    # then set the current & pointer vals
+    current = head
+    pointer = head.next
+
+    while pointer.next:
+        # once the head is ok, check the other nodes
+        if dupes_dict[pointer.data] > 1:
+            current.next = pointer.next
+            current.next
+            pointer.next
+
+
+# call the function
+print(ll_remove_dupes(headnode))
+    # should return the *new* head which will be 10
